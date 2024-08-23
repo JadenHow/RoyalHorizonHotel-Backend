e33dfe912aa49d58c017de41803e2e5d59e5dd59
JadenHow <jaden.how@myblueprint.ca>
2024-08-23 00:50:03 -0400

    add access point

--- src/main/java/com/hotel/RoyalHorizonHotel_Backend/security/CorsConfig.java
+++ src/main/java/com/hotel/RoyalHorizonHotel_Backend/security/CorsConfig.java
@@ -24,7 +24,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000, https://royalhorizonhotel-frontend-s5k2dwd5ma-uc.a.run.app"));
 
         config.setAllowedHeaders(List.of(
                 HttpHeaders.AUTHORIZATION,
